c7170e13df18f65a0e326fa279ccc0f9e39bc684
MICHAEL JOHN P. NGAYAWON <2222574@slu.edu.ph>
2025-07-11 14:15:12 +0800

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,92 +1,94 @@
-'use strict';
+"use strict";
 
 // add event on multiple elements
 const addEventOnElem = function (elements, eventType, callback) {
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].addEventListener(eventType, callback)
-    }
-}
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
 
-const $header = document.querySelector('[data-header]');
-const $navbar = document.querySelector('[data-navbar]');
-const $navToggler = document.querySelectorAll('[data-nav-toggler]');
-const $overlay = document.querySelector('[data-overlay]');
-const $dropdownToggler = document.querySelector('[data-dropdown-toggler]');
-const $dropdown = document.querySelector('[data-dropdown]');
-const $cartToggler = document.querySelector('[data-cart-toggler]');
-const $cartModal = document.querySelector('[data-cart-modal]');
+const $header = document.querySelector("[data-header]");
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelectorAll("[data-nav-toggler]");
+const $overlay = document.querySelector("[data-overlay]");
+const $dropdownToggler = document.querySelector("[data-dropdown-toggler]");
+const $dropdown = document.querySelector("[data-dropdown]");
+const $cartToggler = document.querySelector("[data-cart-toggler]");
+const $cartModal = document.querySelector("[data-cart-modal]");
 
 const toggleNavbar = function () {
-    $navbar.classList.toggle('active');
-    $overlay.classList.toggle('active');
-    document.body.classList.toggle('active');
-}
+  $navbar.classList.toggle("active");
+  $overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+};
 
-addEventOnElem($navToggler, 'click', toggleNavbar);
+addEventOnElem($navToggler, "click", toggleNavbar);
 
 // element toggle function
 const toggleElem = function (elem) {
-    elem.classList.toggle('active');
-} 
+  elem.classList.toggle("active");
+};
 
 // toggle dropdown
-$dropdownToggler.addEventListener('click', function () {
-    toggleElem($dropdown);
+$dropdownToggler.addEventListener("click", function () {
+  toggleElem($dropdown);
 });
 
 // toggle cart
-$cartToggler.addEventListener('click', function () {
-    toggleElem($cartModal);
+$cartToggler.addEventListener("click", function () {
+  toggleElem($cartModal);
 });
 
 // header active when windows scrollY 50px
 const activeHeader = function () {
-    window.scrollY > 50 ? $header.classList.add('active') : $header.classList.remove('active');
-}
+  window.scrollY > 50
+    ? $header.classList.add("active")
+    : $header.classList.remove("active");
+};
 
-window.addEventListener('scroll', activeHeader);
+window.addEventListener("scroll", activeHeader);
 
 // Custom Slider
-const $sliderContainers = document.querySelectorAll('[data-slider-container]');
+const $sliderContainers = document.querySelectorAll("[data-slider-container]");
 
-function sliderInitial ($sliderContainers) {
-    const $slider = $sliderContainers.querySelector('[data-slider]');
-    const $backBtn = $sliderContainers.querySelector('[data-prev-btn]'); 
-    const $nextBtn = $sliderContainers.querySelector('[data-next-btn]'); 
+function sliderInitial($sliderContainers) {
+  const $slider = $sliderContainers.querySelector("[data-slider]");
+  const $backBtn = $sliderContainers.querySelector("[data-prev-btn]");
+  const $nextBtn = $sliderContainers.querySelector("[data-next-btn]");
 
-    function nextSlide() {
-        $slider.appendChild($slider.firstElementChild);
-    }
-    $nextBtn.addEventListener('click', nextSlide);
+  function nextSlide() {
+    $slider.appendChild($slider.firstElementChild);
+  }
+  $nextBtn.addEventListener("click", nextSlide);
 
-    function prevSlide() {
-        $slider.prepend($slider.lastElementChild);
-    }
-    $backBtn.addEventListener('click', prevSlide);
+  function prevSlide() {
+    $slider.prepend($slider.lastElementChild);
+  }
+  $backBtn.addEventListener("click", prevSlide);
 
-    let autoSlideIntervalID;
+  let autoSlideIntervalID;
 
-    function autoSlide() {
-        autoSlideIntervalID = setInterval(function () {
-            nextSlide();
-        }, 2000);
-    }
+  function autoSlide() {
+    autoSlideIntervalID = setInterval(function () {
+      nextSlide();
+    }, 2000);
+  }
 
-    autoSlide();
+  autoSlide();
 
-    function deleteAutoSliding() {
-        clearInterval(autoSlideIntervalID);
-    }
+  function deleteAutoSliding() {
+    clearInterval(autoSlideIntervalID);
+  }
 
-    $slider.addEventListener('mouseover', deleteAutoSliding);
-    $backBtn.addEventListener('mouseover', deleteAutoSliding);
-    $nextBtn.addEventListener('mouseover', deleteAutoSliding);
+  $slider.addEventListener("mouseover", deleteAutoSliding);
+  $backBtn.addEventListener("mouseover", deleteAutoSliding);
+  $nextBtn.addEventListener("mouseover", deleteAutoSliding);
 
-    $slider.addEventListener('mouseout', autoSlide);
-    $backBtn.addEventListener('mouseout', autoSlide);
-    $nextBtn.addEventListener('mouseout', autoSlide);
+  $slider.addEventListener("mouseout", autoSlide);
+  $backBtn.addEventListener("mouseout", autoSlide);
+  $nextBtn.addEventListener("mouseout", autoSlide);
 }
 
 for (let i = 0; i < $sliderContainers.length; i++) {
-    sliderInitial($sliderContainers[i]);
+  sliderInitial($sliderContainers[i]);
 }
